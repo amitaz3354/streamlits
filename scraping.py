07138c3c8c06a384eba395272f1445bb29b4f5a9
amitaz3354 <51985287+amitaz3354@users.noreply.github.com>
2021-01-06 20:28:40 +0200

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import streamlit as st
-from textblob import TextBlobthon
+from textblob import TextBlob
 
 def main():
     #scrape the shit
